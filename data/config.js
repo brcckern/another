bb5ae71c75c67aa2283b3109c22d74b0bc255ac3
brcckern <birciceke@gmail.com>
2024-05-31 23:52:33 +0300

    another

---
+++ data/config.js
@@ -0,0 +1,12 @@
+const config = {
+    db: {
+        host: process.env.MYSQLHOST || "localhost",
+        user: process.env.MYSQLUSER ||  "root",
+        password: process.env.MYSQLPASSWORD || "erenbircicek",
+        database: process.env.MYSQLDATABASE || "esinav",
+        port: process.env.MYSQLPORT || 3306,
+        dialect: "mysql"
+    }
+};
+
+module.exports = config;
